4b90635e27f9c196a8009422227da4f2625134a1
penvarict <penvarict@s.dcsdk12.org>
2017-10-02 11:12:46 -0600

    Added all testing

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -1,10 +1,88 @@
   
 # TODO - write has_teen?
+def has_teen?(array)
+    teens = 0
+    array.each do |number|
+        if number == 13 || number == 14 ||number == 15 || number == 16 ||number == 17 || number == 18 || number == 19
+            teens += 1
+        end
+    end
+
+    if teens > 0
+        return true
+    else 
+        return false
+    end
+
+    
+end
+
+
 
 # TODO - write not_string
+def not_string (str)
+
+    if str[0..2] == "not"
+        return str
+    else
+        return "not#{str}"
+    end
+
+
+
+end
+
+
+
 
 # TODO - write icy_hot?
+def icy_hot?(tempOne, tempTwo)
+
+    if tempOne > 100 || tempTwo > 100
+        return true
+    end
+
+    if tempOne < 0 || tempTwo < 0
+        return true
+    
+    else 
+        return false
+    end
+
+
+
+end
 
 # TODO - write closer_to
 
+def closer_to(target, g1,g2)
+    disg1 = (target - g1).abs
+    disg2 = (target - g2).abs
+
+    if disg1 > disg2
+        return g2
+    elsif disg2 > disg1
+        return g1
+    elsif disg1 == disg2
+        return 0
+    end
+
+
+
+end
+
 # TODO - write two_as_one?
+
+
+def two_as_one?(n1,n2,sum)
+    added = n1+n2 
+    if added == sum
+        return true
+    else
+        return false
+    
+    end
+
+
+
+end
--- test.rb
+++ test.rb
@@ -3,3 +3,110 @@ require './methods_quiz'
 
 # TODO - write tests here
 
+describe 'methods_quiz'do
+
+    describe 'has_teen?' do
+        it 'are any numbers teen'do
+            has_teen?([5,6,13]).must_equal(true)
+        end
+
+        it 'are any numbers teen'do
+            has_teen?([5,6,1]).must_equal(false)
+        end
+        it 'are any numbers teen'do
+            has_teen?([5,19,13]).must_equal(true)
+        end
+
+
+    end
+
+    describe 'not_string' do
+        it 'add front to not in string' do
+            not_string("computer").must_equal("notcomputer")
+        end   
+        it 'add front to not in string' do
+            not_string("notyomama").must_equal("notyomama")
+        end   
+
+        it 'add front to not in string' do
+            not_string("not").must_equal("not")
+        end   
+
+    end
+    describe 'icy_hot?' do
+        it 'is it icy hot' do
+            icy_hot?(-20,120).must_equal(true)
+        end
+        it 'is it icy hot' do
+            icy_hot?(20,120).must_equal(true)
+        end
+        it 'is it icy hot' do
+            icy_hot?(0,0).must_equal(false)
+        end
+        it 'is it icy hot' do
+            icy_hot?(12,21).must_equal(false)
+        end
+        it 'is it icy hot' do
+            icy_hot?(-20,-300).must_equal(true)
+        end
+
+
+
+    end
+
+    describe 'closer_to' do
+        it 'which is closer to target number' do
+            closer_to(23,21,9).must_equal(21)
+        end
+
+        it 'which is closer to target number' do
+            closer_to(24,21,21).must_equal(0)
+        end
+
+        it 'which is closer to target number' do
+            closer_to(20,30,10).must_equal(0)
+        end
+
+        it 'which is closer to target number' do
+           closer_to(1,-2,9).must_equal(-2)
+
+        end
+
+
+    end
+
+    describe 'two_as_one?' do
+        it 'do the first two add to the 3rd?'do
+            two_as_one?(1,2,3).must_equal(true)
+        end
+
+        it 'do the first two add to the 3rd?'do
+            two_as_one?(2,2,3).must_equal(false)
+        end
+
+        it 'do the first two add to the 3rd?'do
+            two_as_one?(0,1,1).must_equal(true)
+        end
+        it 'do the first two add to the 3rd?'do
+            two_as_one?(2000,1000,3000).must_equal(true)
+        end
+
+        it 'do the first two add to the 3rd?'do
+            two_as_one?(-21,-1,-22).must_equal(true)
+        end
+
+        it 'do the first two add to the 3rd?'do
+            two_as_one?(-21,1,-20).must_equal(true)
+        end
+
+        
+
+
+    end
+
+
+
+
+
+
+end
